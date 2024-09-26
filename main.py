47d8f8a16f00238fe3bf6b85cf35e8625637e5b1
SATsyganov <=>
2024-09-26 16:51:59 +0300

    Join commit

--- main.py
+++ main.py
@@ -4,14 +4,16 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
+def print_hit(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def my_func():
+    pass
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Hello')
+    print_hit('Hello')
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
