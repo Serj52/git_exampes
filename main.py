f1be7bfd8e5d774a197570566e179b5b75d20d39
SATsyganov <=>
2024-09-27 14:49:29 +0300

    remove double string

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def print_hit(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hit('Hello')
-    print_hit('Hello')
+
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
